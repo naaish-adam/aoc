faf2e323800e6449aa17bb5e3551fe71e8374963
Naaish Adam <77376880+naaish-adam@users.noreply.github.com>
2024-12-01 17:10:49 +0500

    2024 init

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-mod day9;
+mod day1;
 
 use std::fs;
 
 fn main() {
     let contents =
-        fs::read_to_string("inputs/day9.txt").expect("Something went wrong reading the file");
+        fs::read_to_string("inputs/day1.txt").expect("Something went wrong reading the file");
 
-    day9::part1(&contents);
-    day9::part2(&contents);
+    day1::part1(&contents);
+    day1::part2(&contents);
 }
